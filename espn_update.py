6a8c18e7bededf9006454e2624d5ab1aa94be4c1
patrickjcash <4813177+patrickjcash@users.noreply.github.com>
2023-08-21 14:35:40 -0400

    Fixed league_id command line input

--- espn_update.py
+++ espn_update.py
@@ -5,16 +5,27 @@ from datetime import datetime
 import config  # Import the configuration from config.py
 
 def fetch_and_upload_data(year, league_id, headers, cookies, json_data):
-    # Rest of the function remains the same
+    api_url = f"https://lm-api-writes.fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}/teams/14"
+    
+    response = requests.post(api_url, json=json_data, headers=headers, cookies=cookies)
+    
+    if response.status_code == 200:
+        print("Data uploaded successfully.")
+    else:
+        print(f"Error uploading data. Status code: {response.status_code}")
+        print("Response content:")
+        print(response.content)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Upload data to API from CSV file')
     parser.add_argument('--csv', required=True, help='Path to the CSV file containing data')
     parser.add_argument('--year', default=str(datetime.now().year), help='Year for the API endpoint')
+    parser.add_argument('--league_id', default=config.league_id, help='League ID for the API endpoint (default from config)')
     args = parser.parse_args()
 
     csv_path = args.csv
     year = args.year
+    league_id = args.league_id
 
     json_data = {'draftStrategy': {'draftList': [], 'excludedPlayerIds': []}}
     with open(csv_path, 'r') as csvfile:
@@ -25,4 +36,4 @@ if __name__ == '__main__':
                 'playerId': int(row['playerId']),
             })
 
-    fetch_and_upload_data(year, config.league_id, config.headers, config.cookies, json_data)
+    fetch_and_upload_data(year, league_id, config.headers, config.cookies, json_data)
